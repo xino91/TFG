10103d08c96fe34d82c67b09e154bb3f5a5e4c22
ANTONIO <antonio_aariza@hotmail.com>
2022-10-13 17:06:37 +0200

    Funcinalidad seleccion imagen de perfil

--- app/src/main/java/com/example/apprpe/ui/Perfil/PerfilFragment.java
+++ app/src/main/java/com/example/apprpe/ui/Perfil/PerfilFragment.java
@@ -1,26 +1,29 @@
 package com.example.apprpe.ui.Perfil;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.apprpe.R;
+import com.github.dhaval2404.imagepicker.ImagePicker;
 
-import org.jetbrains.annotations.NotNull;
-
+import java.io.File;
+import java.net.URI;
 import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PerfilFragment extends Fragment {
 
@@ -35,6 +38,8 @@ public class PerfilFragment extends Fragment {
     String email;
     String tipo_actividad;
     String nacimiento;
+    String path;
+    CircleImageView imagen;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -44,11 +49,17 @@ public class PerfilFragment extends Fragment {
         obtenerDatosFicheroPreferencias();
         vistas();
 
+        imagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                elegirImagen();
+            }
+        });
+
         return root;
     }
 
     private void inicializarComponenetes(View root) {
-
         txtView_Usuario = root.findViewById(R.id.textView_Nombre);
         txtView_Genero = root.findViewById(R.id.textView_Genero);
         txtView_Estatura = root.findViewById(R.id.textView_Estatura);
@@ -56,10 +67,12 @@ public class PerfilFragment extends Fragment {
         txtView_Email = root.findViewById(R.id.textView_Email);
         txtView_Actividad = root.findViewById(R.id.textView_Actividad);
         txtView_Nacimiento = root.findViewById(R.id.TextView_fecha);
+        imagen = root.findViewById(R.id.profile_image);
     }
 
     private void obtenerDatosFicheroPreferencias() {
         preferencias = requireActivity().getSharedPreferences("PREFERENCIAS", Context.MODE_PRIVATE);
+
         nombreUsuario = preferencias.getString("NombreUsuario", "");
         genero = preferencias.getString("Genero", "");
         estatura = preferencias.getString("Estatura", "");
@@ -67,6 +80,7 @@ public class PerfilFragment extends Fragment {
         email = preferencias.getString("Email", "");
         tipo_actividad = preferencias.getString("Actividad", "");
         nacimiento = preferencias.getString("Fecha", "");
+        path = preferencias.getString("Path","");
     }
 
     private void vistas(){
@@ -77,6 +91,35 @@ public class PerfilFragment extends Fragment {
         txtView_Email.setText(email);
         txtView_Actividad.setText(tipo_actividad);
         txtView_Nacimiento.setText(nacimiento);
+        CargarImagen();
+    }
+
+    private void CargarImagen() {
+        File file = new File(path);
+        imagen.setImageURI(Uri.fromFile(file));
+    }
+
+    private void elegirImagen() {
+        ImagePicker.Companion.with(this)
+                .galleryOnly()
+                .crop()
+                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK) {
+            Uri uri = Objects.requireNonNull(data).getData();
+            imagen.setImageURI(uri);
+
+            preferencias = requireActivity().getSharedPreferences("PREFERENCIAS", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferencias.edit();
+            editor.putString("Path", uri.getEncodedPath());
+            editor.apply();
+            Log.e("Hola2", uri.getEncodedPath());
+        }
     }
 
 }
